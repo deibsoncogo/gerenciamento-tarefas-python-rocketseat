477cd8fc29c3e8c4222649df5f49493c3a97dd02
Deibson Cogo <deibsoncogo@outlook.com>
2025-01-06 21:42:48 -0300

    feat: criação da rota create task

--- app.py
+++ app.py
@@ -1,14 +1,26 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from models.task import Task
 
 app = Flask(__name__)
 
-@app.route("/")
-def helloWorld():
-  return "Hello world"
+tasks = []
+taskIdControl = 1
 
-@app.route("/about")
-def about():
-  return "Página sobre"
+@app.route("/tasks", methods=["POST"])
+def createTask():
+  global taskIdControl
+
+  data = request.get_json()
+
+  newTask = Task(
+    id=taskIdControl, title=data["title"], description=data.get("description", "")
+  )
+
+  taskIdControl += 1
+
+  tasks.append(newTask)
+
+  return jsonify({ "message": "Nova tarefa criada com sucesso" })
 
 if __name__ == "__main__":
   app.run(debug=True)
